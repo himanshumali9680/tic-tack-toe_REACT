714139026ef7a8179cf1885dd272d0c83c8c863a
himanshumali9680 <himanshumali9680@gmail.com>
2024-02-28 17:52:00 +0530

    userinput and twoway-binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initalName="Player 1" symbol="X"/>
+          <Player initalNameame="Player 2" symbol="0"/>
+
+        </ol>
+        PLAYERS GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    
+  const[playerName, setIsPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setIsPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
